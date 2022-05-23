f4acd999ade4e6cdda21493ecc073194e5706cc7
Franco <elecman@live.com.ar>
2022-05-23 20:46:46 -0300

    Creacion de proyecto

---
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/All4petsApplication.java
@@ -0,0 +1,13 @@
+package com.all4pets.Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class All4petsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(All4petsApplication.class, args);
+	}
+
+}
