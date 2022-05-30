df145cfd413016330f578f45853caac670bb95f5
Franco <elecman@live.com.ar>
2022-05-30 20:34:20 -0300

    Completado de entidad producto y repositorio

---
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/entidades/enumeraciones/servicios/ProductoRepositorio.java
@@ -0,0 +1,10 @@
+
+package com.all4pets.Final.entidades.enumeraciones.servicios;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductoRepositorio {
+    
+}
