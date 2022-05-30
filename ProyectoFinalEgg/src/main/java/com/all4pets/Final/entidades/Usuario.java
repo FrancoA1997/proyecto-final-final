7cf8312b976c558d171c0aeb501b1effc6425893
SebaHubaide <shubaide@gmail.com>
2022-05-30 20:08:30 -0300

    Entidad usuario

--- ProyectoFinalEgg/src/main/java/com/all4pets/Final/entidades/Usuario.java
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/entidades/Usuario.java
@@ -1,7 +1,20 @@
 
 package com.all4pets.Final.entidades;
 
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
 public class Usuario {
-    private String atributofalsoSeba;
-    private String pepe;
+  @Id  
+    private String id;
+    private String nombreYapellido;
+    private int edad;
+    private Mascota mascota;
+    private String email;
+    private String contraseña;
+    private String telefono;
+    private String direccion;
+    private Boolean alta;
+    
 }
