9a5e0d4c5d1bcb4ea863af4bd7c273c03e0dc43c
Agustin Franco <agustinhernan_10@hotmail.com>
2022-06-05 17:10:16 -0300

    Update MascotaRepo
    Incorporación de librerías y método básico

--- ProyectoFinalEgg/src/main/java/com/all4pets/Final/entidades/repositorios/MascotaRepositorio.java
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/entidades/repositorios/MascotaRepositorio.java
@@ -1,10 +1,16 @@
-
 package com.all4pets.Final.entidades.repositorios;
 
+import com.all4pets.Final.entidades.Mascota;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface MascotaRepositorio  {
+public interface MascotaRepositorio extends JpaRepository<Mascota, String> {
+    
+    @Query("SELECT a FROM Mascota a WHERE a.usuario.id = :id")
+    public List<Mascota> buscarMascotasPorUsuario(@Param("id") String id);
     
 }
