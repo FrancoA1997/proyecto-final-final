2b73f67555cb03e650061d2f6153d67f50c6789f
SebaHubaide <shubaide@gmail.com>
2022-06-07 20:34:58 -0300

    MascotaServicio + Imágen

--- ProyectoFinalEgg/src/main/java/com/all4pets/Final/servicios/MascotaServicio.java
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/servicios/MascotaServicio.java
@@ -1,14 +1,17 @@
 package com.all4pets.Final.servicios;
 
+import com.all4pets.Final.entidades.Imagen;
 import com.all4pets.Final.entidades.Mascota;
 import com.all4pets.Final.enumeraciones.Edad;
 import com.all4pets.Final.enumeraciones.Estado;
 import com.all4pets.Final.enumeraciones.Genero;
 import com.all4pets.Final.excepciones.ExcepcionPropia;
 import com.all4pets.Final.repositorios.MascotaRepositorio;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class MascotaServicio {
@@ -16,8 +19,11 @@ public class MascotaServicio {
     @Autowired
     private MascotaRepositorio mascotaRepo;
 
+    @Autowired
+    private ImagenServicio imagenServicio;
+
     @Transactional
-    public Mascota registrar(String tipo, String observacion, Estado estado, Edad edad, Genero genero, Boolean estaActivo) throws ExcepcionPropia {
+    public Mascota registrar(String tipo, String observacion, Estado estado, Edad edad, Genero genero, Boolean estaActivo, MultipartFile archivo) throws ExcepcionPropia {
         validar(tipo, observacion, estado, edad, genero);
         Mascota mascota = new Mascota();
         mascota.setTipo(tipo);
@@ -26,6 +32,8 @@ public class MascotaServicio {
         mascota.setEdad(edad);
         mascota.setGenero(genero);
         mascota.setAlta(estaActivo);
+        Imagen imagen = imagenServicio.multiPartToEntity(archivo);
+        mascota.setImagen(imagen);
 
         return mascotaRepo.save(mascota);
     }
@@ -49,4 +57,27 @@ public class MascotaServicio {
         }
     }
 
+    public void deshabilitar(String id) throws ExcepcionPropia {
+
+        Optional<Mascota> respuesta = mascotaRepo.findById(id);
+        if (respuesta.isPresent()) {
+            Mascota mascota = respuesta.get();
+            mascota.setAlta(Boolean.FALSE);
+            mascotaRepo.save(mascota);
+        } else {
+            throw new ExcepcionPropia("No se encontro la mascota solicitada");
+        }
+    }
+
+    public void habilitar(String id) throws ExcepcionPropia {
+
+        Optional<Mascota> respuesta = mascotaRepo.findById(id);
+        if (respuesta.isPresent()) {
+            Mascota mascota = respuesta.get();
+            mascota.setAlta(Boolean.TRUE);
+            mascotaRepo.save(mascota);
+        } else {
+            throw new ExcepcionPropia("No se encontro el usuario solicitado");
+        }
+    }
 }
