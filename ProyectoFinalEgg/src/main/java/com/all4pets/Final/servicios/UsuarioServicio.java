134bc9d212e202e50fe7c01d4d2f590b2cbb167e
Agustin Franco <agustinhernan_10@hotmail.com>
2022-06-07 19:24:25 -0300

    Update UsuarioServicio.java
    Se agrego método modificar usuario

--- ProyectoFinalEgg/src/main/java/com/all4pets/Final/servicios/UsuarioServicio.java
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/servicios/UsuarioServicio.java
@@ -2,10 +2,12 @@ package com.all4pets.Final.servicios;
 
 import com.all4pets.Final.entidades.Usuario;
 import com.all4pets.Final.enumeraciones.Rol;
+import com.all4pets.Final.enumeraciones.Sexo;
 import com.all4pets.Final.excepciones.ExcepcionPropia;
 import com.all4pets.Final.repositorios.UsuarioRepositorio;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,41 +30,60 @@ public class UsuarioServicio implements UserDetailsService {
 
     @Transactional
     public Usuario registrar(String nombre, String email, String clave) throws ExcepcionPropia {
-        
+
         validar(nombre, email, clave);
-        
+
         Usuario usuario = new Usuario();
-        
+
         usuario.setNombre(nombre);
         usuario.setEmail(email);
         usuario.setAlta(Boolean.TRUE);
         usuario.setRol(Rol.USUARIO);
-        
+
         String claveEncriptada = new BCryptPasswordEncoder().encode(clave);
         usuario.setClave(claveEncriptada);
-        
+
         return usuarioRepo.save(usuario);
     }
 
-    public void validar(String nombre, String email, String clave) throws ExcepcionPropia {
+    public void modificar(String id, String nombre, String email, String clave, Sexo sexo, Integer edad) throws ExcepcionPropia {
+
+        validar(nombre, email, clave);
         
+        Optional<Usuario> respuesta = usuarioRepo.findById(id);
+        if (respuesta.isPresent()) {
+            Usuario usuario = respuesta.get();
+
+            usuario.setNombre(nombre);
+            usuario.setEmail(email);
+            usuario.setSexo(sexo);
+            usuario.setEdad(edad);
+
+            String claveEncriptada = new BCryptPasswordEncoder().encode(clave);
+            usuario.setClave(claveEncriptada);
+        }
+
+    }
+
+    private void validar(String nombre, String email, String clave) throws ExcepcionPropia {
+
         if (nombre == null || nombre.trim().isEmpty()) {
             throw new ExcepcionPropia("Por favor, indique su nombre");
         }
-        
+
         if (email == null || email.trim().isEmpty()) {
             throw new ExcepcionPropia("Por favor, indique su email");
         }
-        
+
         if (clave == null || clave.trim().isEmpty()) {
             throw new ExcepcionPropia("Por favor, indique su clave");
         }
-        
+
     }
-    
+
     @Override
     public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
-        
+
         Usuario usuario = usuarioRepo.buscarPorEmail(mail);
         if (usuario != null) {
             List<GrantedAuthority> permisos = new ArrayList<>();
