e7d357a82b6438cc50843b089f66a19524fd224a
Agustin Franco <agustinhernan_10@hotmail.com>
2022-06-07 21:30:03 -0300

    Metodo Actualizar Imagen
    Metodo comentado para actualizar solo la foto de perfil

--- ProyectoFinalEgg/src/main/java/com/all4pets/Final/servicios/ImagenServicio.java
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/servicios/ImagenServicio.java
@@ -4,6 +4,7 @@ import com.all4pets.Final.entidades.Imagen;
 import com.all4pets.Final.excepciones.ExcepcionPropia;
 import com.all4pets.Final.repositorios.ImagenRepositorio;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,4 +36,30 @@ public class ImagenServicio {
         }
         
     }
+    
+// METODO PARA CAMBIAR 'SOLO' LA FOTO DE PERFIL DEL USUARIO O MASCOTA (TO DO)
+    
+//    public Imagen actualizar(String idImagen, MultipartFile archivo) throws ExcepcionPropia {
+//        
+//        if(archivo != null) {
+//            Imagen imagen = new Imagen();
+//            if(idImagen != null) {
+//                Optional<Imagen> respuesta = imagenRepo.findById(idImagen);
+//                if(respuesta.isPresent()) {
+//                    imagen = respuesta.get();
+//                }
+//            }
+//            imagen.setMime(archivo.getContentType());
+//            imagen.setNombre(archivo.getName());
+//            try {
+//                imagen.setContenido(archivo.getBytes());
+//            } catch (IOException ex) {
+//                Logger.getLogger(ImagenServicio.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            return imagenRepo.save(imagen);
+//        } else {
+//            throw new ExcepcionPropia("No se puede cargar la foto");
+//        }
+//        
+//    }
 }
