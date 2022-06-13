1c7c59e6c923ca643e2f3b7153ec61166489d392
Agustin Franco <agustinhernan_10@hotmail.com>
2022-06-13 17:36:11 -0300

    Update UsuarioControlador.java
    metodo modificar

--- ProyectoFinalEgg/src/main/java/com/all4pets/Final/controladores/UsuarioControlador.java
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/controladores/UsuarioControlador.java
@@ -1,5 +1,6 @@
 package com.all4pets.Final.controladores;
 
+import com.all4pets.Final.enumeraciones.Sexo;
 import com.all4pets.Final.excepciones.ExcepcionPropia;
 import com.all4pets.Final.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("usuario")
@@ -44,8 +46,20 @@ public class UsuarioControlador {
     }
 
     @PostMapping("modificar")
-    public String modificar() {
-        return "perfilUsuario.html";
+    public String modificar(ModelMap modelo, @RequestParam String id, @RequestParam Sexo sexo, @RequestParam Integer edad, @RequestParam String telefono, @RequestParam String direccion, @RequestParam MultipartFile archivo) throws ExcepcionPropia {
+        
+        try {
+            usuarioServicio.modificar(id, sexo, edad, telefono, direccion, archivo);
+        } catch (ExcepcionPropia e) {
+            modelo.put("error", e.getMessage());
+            modelo.put("sexo", sexo);
+            modelo.put("edad", edad);
+            modelo.put("telefono", telefono);
+            modelo.put("direccion", direccion);
+        }
+
+        modelo.put("exito", "Modificacion realizada con éxito");
+        return "perfil.html";
     }
 
 }
