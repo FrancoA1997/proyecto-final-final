942d950376b905294f6c3693c2a4bd132ba1d907
Agustin Franco <agustinhernan_10@hotmail.com>
2022-06-16 19:08:01 -0300

    Update Estado.java
    cambio estetico

--- ProyectoFinalEgg/src/main/java/com/all4pets/Final/enumeraciones/Estado.java
+++ ProyectoFinalEgg/src/main/java/com/all4pets/Final/enumeraciones/Estado.java
@@ -5,16 +5,6 @@ public enum Estado {
     PERDIDO("PERDIDO"),
     ADOPCION("ADOPCION");
 
-//    private final String displayValue;
-//
-//    private Estado(String displayValue) {
-//        this.displayValue = displayValue;
-//    }
-//
-//    public String getDisplayValue() {
-//        return displayValue;
-//    }
-
     private String nombre;
 
     private Estado(String nombre) {
